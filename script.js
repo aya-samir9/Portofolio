45f3fe6fd9a8e8cf73333ea0df9c56e100f8e4e0
aya-samir9 <aya941142@gmail.com>
2024-12-15 12:37:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleClickHere() {
+    const content = document.getElementById("content");
+    content.classList.toggle("active");
+    alert("Welcome to Mohamed Salah's Portfolio!");
+}
